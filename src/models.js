7bdf72fae02ae9c6f51c02628c1d133ee201bb9e
musikisum <ulrich.kaiser@hmtm.de>
2023-07-22 23:21:56 +0200

    Design a new phrase model object

--- src/models.js
+++ src/models.js
@@ -1,8 +1,19 @@
+export const ModelProtoA = {
+  name: '',
+  displayName: '',
+  description: '',
+  voiceUpper: ['e2', 'd4', 'c2'],
+  voiceInner: ['g2', 'f4', 'e2'],
+  voiceLower: ['C2', 'C2', 'B2', 'C2']
+}
+
+// TODO Implement algorithm to generate an array for Playback function    
+
 export const ModelA = [
   [['g', 'e', 'C', 2]],
   [['f', 'd', 4], ['C', 2]],
   [['B,', 2]],
-  [['e', 'c', 'C', 2]],
+  [['e', 'c', 'C', 2]]
 ]
 
 export const ModelB = [
